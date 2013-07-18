1a829b194012926ecb9e5f26d1b348bdc07c2215
taintech <rinattainov@gmail.com>
2013-07-19 05:19:50 +0600

    my first commit

---
+++ src/main/scala/com/taintech/krisha/dataminer/App.scala
@@ -0,0 +1,19 @@
+package com.taintech.krisha.dataminer
+
+/**
+ * Author: Rinat Tainov
+ * Email: rinat@tainov.com
+ * Date: 7/19/13
+ * Time: 5:03 AM
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+
+  }
+
+}
